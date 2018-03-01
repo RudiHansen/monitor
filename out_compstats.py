dc192a21bd7d6efd1d784a3fd1de248d68a60594
RudiHansen <rsh@pobox.com>
2018-03-01 11:28:48 +0100

    Rettelser

--- out_compstats.py
+++ out_compstats.py
@@ -1,58 +1,76 @@
 #!/usr/bin/python
+# coding=utf-8
 import csv
 import collections
 from datetime import datetime
 from datetime import timedelta
 from colorama import Fore, Back, Style
+import os
+import time
 
 CompStats = collections.namedtuple('CompStats', 'ComputerName ComputerDescription ComputerOS Location IPInternal IPExternal LastOnlineDateTime UpdateIntervalSec CPUUtilization DiskUtilization')
 
-#print 'Type of CompStats', type(CompStats)
-
-#datetime_object  = datetime.strptime('2015-08-15 20:40:00', '%Y-%m-%d %H:%M:%S')
-
-#comp1 = CompStats(ComputerName='Test1',ComputerDescription='',ComputerOS='',Location='', IPInternal='',IPExternal='',LastOnlineDateTime=datetime_object,UpdateIntervalSec=0,CPUUtilization=0,DiskUtilization=0)
-#comp2 = CompStats(ComputerName='Test2',ComputerDescription='',ComputerOS='',Location='', IPInternal='',IPExternal='',LastOnlineDateTime='',UpdateIntervalSec=0,CPUUtilization=0,DiskUtilization=0)
-
-#print 'Comp1 = ', comp1.ComputerName
-#print 'Comp1 = ', comp1.LastOnlineDateTime
-#print 'Type',type(comp1.LastOnlineDateTime)
-#print 'Comp2 = ', comp2.ComputerName
-#print 'Comp2 = ', comp2.LastOnlineDateTime
-
-with open('compstats.csv', 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=';', quotechar='"')
-    rowcount = 1
-    computer_list = []
-    for row in spamreader:
-        if(rowcount <> 1):
-            computer = CompStats(ComputerName=row[0],
-                                 ComputerDescription=row[1],
-                                 ComputerOS=row[2],
-                                 Location=row[3], 
-                                 IPInternal=row[4],
-                                 IPExternal=row[5],
-                                 LastOnlineDateTime=datetime.strptime(row[6], '%d-%m-%Y %H:%M:%S'),
-                                 UpdateIntervalSec=int(row[7]),
-                                 CPUUtilization=int(row[8]),
-                                 DiskUtilization=int(row[9]))
-            computer_list.append(computer)
-        rowcount+=1
-        
-
-dateTimeNow = datetime.now()
-outString = '{0:<15} {1:20} {2:5} {3:5}'.format('ComputerName','LastOnlineDateTime','CPU','Disk')
-print outString
-for computer in computer_list:
-    lastOnlineDataTime     = computer.LastOnlineDateTime + timedelta(seconds=computer.UpdateIntervalSec)
-    lastOnlineDataTimeLong = computer.LastOnlineDateTime + timedelta(seconds=computer.UpdateIntervalSec*5)
+def loadCompStatsFromCsv( fileName ):
+    with open(fileName, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        rowcount = 1
+        computer_list = []
+        for row in spamreader:
+            if(rowcount <> 1):
+                computer = CompStats(ComputerName=row[0],
+                                     ComputerDescription=row[1],
+                                     ComputerOS=row[2],
+                                     Location=row[3], 
+                                     IPInternal=row[4],
+                                     IPExternal=row[5],
+                                     LastOnlineDateTime=datetime.strptime(row[6], '%d-%m-%Y %H:%M:%S'),
+                                     UpdateIntervalSec=int(row[7]),
+                                     CPUUtilization=int(row[8]),
+                                     DiskUtilization=int(row[9]))
+                computer_list.append(computer)
+            rowcount+=1
+    return computer_list
     
-    if(dateTimeNow > lastOnlineDataTimeLong):
-	outString = Back.RED + '{0:<15}'.format(computer.ComputerName) + Back.RESET
-    elif(dateTimeNow > lastOnlineDataTime):
-	outString = Back.YELLOW + '{0:<15}'.format(computer.ComputerName) + Back.RESET
-    else:
-	outString = Back.GREEN + '{0:<15}'.format(computer.ComputerName) + Back.RESET
-        
-    outString += ' {0:20} {1:5} {2:5}'.format(str(computer.LastOnlineDateTime),str(computer.CPUUtilization),str(computer.DiskUtilization))
+def printCompStats( computer_list):
+    dateTimeNow = datetime.now()
+    outString = '{0:<15} {1:20} {2:5} {3:5}'.format('ComputerName','LastOnlineDateTime','CPU','Disk')
     print outString
+    for computer in computer_list:
+        lastOnlineDateTime     = computer.LastOnlineDateTime + timedelta(seconds=computer.UpdateIntervalSec)
+        lastOnlineDateTimeLong = computer.LastOnlineDateTime + timedelta(seconds=computer.UpdateIntervalSec*2)
+    
+        # Set Colors on ComputerName && LastOnlineDateTime
+        if(dateTimeNow > lastOnlineDateTimeLong):
+            ComputerName        = Back.RED + '{0:<15}'.format(computer.ComputerName) + Back.RESET
+            LastOnlineDateTime  = Back.RED + '{0:20}'.format(str(computer.LastOnlineDateTime)) + Back.RESET
+        elif(dateTimeNow > lastOnlineDateTime):
+            ComputerName        = Back.YELLOW + '{0:<15}'.format(computer.ComputerName) + Back.RESET
+            LastOnlineDateTime  = Back.YELLOW + '{0:20}'.format(str(computer.LastOnlineDateTime)) + Back.RESET
+        else:
+            ComputerName        = Back.GREEN + '{0:<15}'.format(computer.ComputerName) + Back.RESET
+            LastOnlineDateTime  = Back.GREEN + '{0:20}'.format(str(computer.LastOnlineDateTime)) + Back.RESET
+        
+        # Set Colors on CPUUtilization
+        if(computer.CPUUtilization >= 90):
+            CPUUtilization = Back.RED + '{0:5}'.format(str(computer.CPUUtilization)) + Back.RESET
+        elif(computer.CPUUtilization >= 80):
+            CPUUtilization = Back.YELLOW + '{0:5}'.format(str(computer.CPUUtilization)) + Back.RESET
+        else:
+            CPUUtilization = Back.GREEN + '{0:5}'.format(str(computer.CPUUtilization)) + Back.RESET
+
+        # Set Colors on DiskUtilization
+        if(computer.DiskUtilization >= 90):
+            DiskUtilization = Back.RED + '{0:5}'.format(str(computer.DiskUtilization)) + Back.RESET
+        elif(computer.DiskUtilization >= 80):
+            DiskUtilization = Back.YELLOW + '{0:5}'.format(str(computer.DiskUtilization)) + Back.RESET
+        else:
+            DiskUtilization = Back.GREEN + '{0:5}'.format(str(computer.DiskUtilization)) + Back.RESET
+        
+        outString = '{0:<15} {1:20} {2:5} {3:5}'.format(ComputerName,LastOnlineDateTime,CPUUtilization,DiskUtilization)
+        print outString
+
+while (1):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    computer_list = loadCompStatsFromCsv('compstats.csv')    
+    printCompStats(computer_list)
+    time.sleep(5)
--- readme.txt
+++ readme.txt
@@ -3,18 +3,21 @@ Planned functionality
 General:
 
 Computer Status:
-    Each computer logs these fields.
-        ComputerName
-        ComputerDescription
-        ComputerOS
-        Location
-        IP Internal
-        IP External
-        LastOnlineDateTime
-        UpdateIntervalSec
-        CPUUtilization
-        DiskUtilization
-        
+    Define namedtuple for ComputerStatus data (CompStats)
+        Fields in namedtuple are:
+            STR         ComputerName
+            STR         ComputerDescription
+            STR         ComputerOS
+            STR         Location
+            STR         IPInternal
+            STR         IPExternal
+            DateTime    LastOnlineDateTime
+            INT         UpdateIntervalSec
+            INT         CPUUtilization
+            INT         DiskUtilization
+    Write compstats.csv file with test data.
+    Read ComputerStatus from csv file and print to terminal
+    Make output to terminal use urwid.
 
 
 Changlog.
